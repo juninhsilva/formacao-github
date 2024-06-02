fa314e72fb22db5a7835dc08904391e6f2ae6db7
Gilmar Pereira da Silva Júnior <juninhsilva@live.com>
2024-06-02 09:22:51 -0300

    desafios

---
+++ desafios/clonando-repositorios.js
@@ -0,0 +1,7 @@
+repository_url = input()
+
+//# TODO: Escreva o comando Git para clonar o repositório
+git_command = "git clone "
+
+// # Imprime o comando Git seguido da URL do repositório recebida como entrada
+print(git_command, repository_url)
